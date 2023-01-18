6a6c21bb28ea45cfe7d48a0f7797f73b49ae4699
junbaibai0719 <44611975+junbaibai0719@users.noreply.github.com>
2023-01-18 21:56:11 +0800

    readlines性能提升以及测试 (#7)
    * doc
    
    * doc: platform
    
    * refactor: refactor dir
    
    * fix:test python native io
    
    * feat: readline readlines
    
    * doc: usage
    
    * perf: 性能优化
    
    * feat: async with
    
    * fix: remove timer import; doc: function 'open' docs
    
    * fix: callback arg none
    
    * feat: setup
    
    * test
    
    * fix: handle removed

--- src/overlapped.pyx
+++ src/overlapped.pyx
@@ -1,10 +1,10 @@
-# cython: language_level=3
+# cython: language_level=3, c_string_type=unicode, c_string_encoding=utf8
 # distutils: language = c++
 
 from ioapi cimport GetOverlappedResult
+cimport cython
 
 cdef class Overlapped:
-
     def __cinit__(self):
         _ov = <LPOVERLAPPED> GlobalAlloc(GPTR, sizeof(LPOVERLAPPED))
 
@@ -16,7 +16,14 @@ cdef class Overlapped:
     def address(self):
         return <unsigned long long> self._lpov
 
-    # def getresult(self):
+    cdef char * getresult_char(self):
+        cdef HANDLE handle = self._lpov.hEvent
+        cdef DWORD transferred = 0;
+        cdef BOOL ret;
+        ret = GetOverlappedResult(handle, self._lpov, &transferred, 1)
+        return self._buffer
+
+
     cpdef bytes getresult(self):
         cdef HANDLE handle = self._lpov.hEvent
         cdef DWORD transferred = 0;
