d0cdf2a9f7d6adf33f47922b7592af388018d466
junbaibai <1355767057@qq.com>
2023-02-09 22:57:22 +0800

    feat: test caio

--- test_write.py
+++ test_write.py
@@ -41,6 +41,22 @@ async def test_aiofiles_write_file():
         await asyncio.gather(*write_tasks)
 
 
+@timer.atimer
+async def test_caio_write_file():
+    from caio import AsyncioContext
+    ctx = AsyncioContext(max_requests=128)
+    with open("write3.txt", "w") as fp:
+        write_tasks = []
+        fd = fp.fileno()
+        offset = 0
+        for i in range(10 ** 6):
+            s = f"{i}" * 100
+            task = ctx.write(s.encode(), fd, offset=offset)
+            offset += len(s)
+            write_tasks.append(task)
+        await asyncio.gather(*write_tasks)
+
+
 @timer.timer
 def valid():
     with open("write.txt", "rb") as fp0, open("write1.txt", "rb") as fp1, open("write2.txt", "rb") as fp2:
@@ -90,6 +106,7 @@ async def test_write_lines():
             # await _write(fp, f"中{i}:{s}\n")
         await fp.write_lines(lines)
 
+
 @timer.timer
 def test_python_write_lines():
     with open("write.txt", "wb") as fp:
@@ -103,9 +120,10 @@ def test_python_write_lines():
 
 if __name__ == '__main__':
     # asyncio.run(test_write_correct_repeat())
-    # asyncio.run(test_write_file())
-    # # asyncio.run(test_aiofiles_write_file())
-    # test_python_write_file()
-    asyncio.run(test_write_lines())
-    test_python_write_lines()
+    asyncio.run(test_write_file())
+    asyncio.run(test_aiofiles_write_file())
+    asyncio.run(test_caio_write_file())
+    test_python_write_file()
+    # asyncio.run(test_write_lines())
+    # test_python_write_lines()
     valid()
