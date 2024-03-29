3e8ff64263e4e7e1d0ed45e852341119112094fd
junbaibai <1355767057@qq.com>
2024-03-29 22:20:38 +0800

    feat: 直接编译，不需要使用Extension

--- setup.py
+++ setup.py
@@ -3,21 +3,11 @@ import pathlib
 from setuptools import Extension, setup, find_packages
 from Cython.Build import cythonize
 
-extensions = [
-    Extension("aiofile", ["src/aiofile.pyx", "./source/futuremap.cpp"], define_macros=[("_AMD64_", None)],
-              include_dirs=["./include"]),
-    Extension("overlapped", ["src/overlapped.pyx"],
-              define_macros=[("_AMD64_", None)],
-              include_dirs=["./include"]),
-    Extension("io_callback", ["src/io_callback.pyx"],
-              define_macros=[("_AMD64_", None)],
-              include_dirs=["./include"]),
-]
 
 setup(
     name="aiofile",
     version="1.0.0",
-    ext_modules=cythonize(extensions, language="c++", annotate=True, language_level='3'),
+    ext_modules=cythonize(["src/aiofile.pyx", "src/overlapped.pyx", "src/io_callback.pyx"], language="c++", annotate=True, language_level='3'),
     zip_safe=False,
     # data_files=[
     #     ("", ["src/__init__.py"])
